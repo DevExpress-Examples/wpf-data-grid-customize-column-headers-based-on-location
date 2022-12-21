48d282a245df28e86c73552bfe55c807c135e2eb
Alexander D <alexandr.demenin@devexpress.com>
2022-12-21 19:29:20 +0400

    Initial commit

---
+++ CS/CustomColumnHeader/CustomHeaderConverter.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CustomColumnHeader
+{
+    public class CustomHeaderConverter : MarkupExtension, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values[0] is string originalHeader && values[1] is HeaderPresenterType headerType) {
+                return GetCustomHeaderString(originalHeader, headerType);
+            }
+
+            return values[0];
+        }
+
+        private string GetCustomHeaderString(string originalHeader, HeaderPresenterType headerType) {
+            switch (headerType) {
+                case HeaderPresenterType.Headers:
+                    return originalHeader.Replace("Original ", "Custom\nPanel\n");
+                case HeaderPresenterType.GroupPanel:
+                    return originalHeader.Replace("Original", "Custom Group");
+                case HeaderPresenterType.ColumnChooser:
+                    return originalHeader.Replace("Original", "Custom Column Chooser");
+            }
+
+            return originalHeader;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ CS/CustomColumnHeader/Item.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+
+namespace CustomColumnHeader
+{
+    public class Item : BindableBase
+    {
+        public string Name {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public decimal Group {
+            get => GetValue<decimal>();
+            set => SetValue(value);
+        }
+
+        public Item(string name, decimal group) {
+            Name = name;
+            Group = group;
+        }
+
+        public static IEnumerable<Item> GetData(int quantity) {
+            var gen = new Random(42);
+
+            for (int i = 0; i < quantity; i++) {
+                var name = $"Name# {i}";
+                var group = gen.Next(1, 4);
+
+                yield return new Item(name, group);
+            }
+        }
+    }
+}
